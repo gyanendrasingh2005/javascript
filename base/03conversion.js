6e72094bb63386bcceeaed3e2b469085c0d51c4a
Gyanendra singh <155981223+gyanendrasingh2005@users.noreply.github.com>
2025-01-29 19:03:28 +0000

    conversion

---
+++ base/03conversion.js
@@ -0,0 +1,26 @@
+let score=null
+console.log(typeof score);
+console.log(typeof(score))
+
+//conversion to int 
+let valueInNUmber=Number(score)
+console.log(typeof(score));
+console.log(valueInNUmber);
+
+//"33"=> 33
+//"33abc" =>NaN
+//true=>1;false=>0
+
+let islogged=1
+let booleanisloggedin=Boolean(islogged)
+console.log(booleanisloggedin)
+
+// 1=> true;  0 =>false
+// " "=>false
+// "gyanendra "=>true
+
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber)
+console.log(typeof(stringnumber))
