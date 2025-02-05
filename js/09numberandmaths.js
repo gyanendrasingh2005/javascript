f9f5f7c2265c02953a80655c19922fde0c90e8ce
Gyanendra singh <155981223+gyanendrasingh2005@users.noreply.github.com>
2025-02-05 09:08:36 +0000

    date and time done

--- js/09numberandmaths.js
+++ js/09numberandmaths.js
@@ -17,8 +17,20 @@
 
 // maths 
 
-console.log(Math.abs(-4));
-console.log(Math.round(4.6));
-console.log(Math.ceil(4.6));
-console.log(Math.floor(4.6));
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4,6,8,6));
+// console.log(Math.max(4,6,8,6));
+// radom ki vakue always b/w 0 and 1
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min=10;
+const max=20;
+
+console.log(Math.random()*(max-min+1)+min);
+
+
 
