334f75f6608710ccf3027d853a844d81d28497c3
Gyanendra singh <155981223+gyanendrasingh2005@users.noreply.github.com>
2025-02-05 08:38:40 +0000

    maths done

--- js/09numberandmaths.js
+++ js/09numberandmaths.js
@@ -1,3 +1,24 @@
-const score=400
-const balance =new Number(100)
-console.log(balance)
+// const score=400
+// const balance =new Number(100)
+// console.log(balance)
+// console.log(balance.toString)
+// // jitne zeros lagane h untna value de doge to apne aap le lega
+// console.log(balance.toFixed(2));
+// const othernuber=23.894;
+// // agar mai 3 deta hu to to 3 wale digit ko ye precise  karega 
+// console.log(othernuber.toPrecision(2))
+
+// const hundreds=100000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+
+// maths 
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+
