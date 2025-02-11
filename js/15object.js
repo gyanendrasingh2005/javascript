76ba4a02cbcb7c458a16500d1c3db40775113dd2
Gyanendra singh <155981223+gyanendrasingh2005@users.noreply.github.com>
2025-02-11 17:36:51 +0000

    object done

---
+++ js/15object.js
@@ -0,0 +1,34 @@
+
+const course={
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "gyanendra"
+}
+
+// course.courseInstructor
+
+// const{courseInstructor}=course
+// console.log(courseInstructor);
+//or 
+const{courseInstructor:instructor}=course
+console.log(instructor);
+              // destructing 
+const navbar = ({company})=>{
+
+}
+navbar(company="gyanendra")
+
+
+// jason 
+{
+    name:"gyanendra ",
+    coursename: "js in hindi",
+    price:"free"
+}
+
+[
+    {},
+    {},
+    {},
+]
+
