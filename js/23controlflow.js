242e94dc4765d4ddff7615e6580a7ebc8480610b
Gyanendra singh <155981223+gyanendrasingh2005@users.noreply.github.com>
2025-04-30 13:31:35 +0000

    controlflowdone

---
+++ js/23controlflow.js
@@ -0,0 +1,140 @@
+// control flow 
+// if statement 
+   
+// if(condition)
+// {
+   
+// }
+const isUserloggedin=true;
+
+// if(2=="2")
+// {  
+//      console.log("executed");
+// }
+// <,>,<=,>=,==,!=,===,!== negavtive check
+// const temperature=80;
+// if(temperature<50)
+//     console.log("temperature is greaterthan50");
+// else
+
+// console.log("temperature is greater than  50 ");
+
+
+// yeha pe agr power ki jagah hm var use karte to o function ke bahar bhi output deta
+// but yeha pr o nhi dega kuki let and const dono function ke under define hote hai 
+
+// const score=200
+// if(score>100)
+// {
+//     const power="fly";
+//     console.log(`user power${power}`);
+// }
+// console.log(`user power${power}`);
+
+
+
+const balance=100;
+// if(balance>500)
+//     console.log("test2");
+
+// if(balance >500)
+// {
+//     console.log("less than");
+
+// }
+// else if(balance<750)
+// {
+//     console.log("less than 750"); 
+// }
+// else if(balance<900)
+//     {
+//         console.log("less than 900"); 
+//     }
+    
+
+const userloggedin=true;
+const debitcard=true;
+const loggedinfromgoogle=true;
+if(userloggedin&&debitcard&&2==2)
+{
+    // console.log("allowed to buy courses");
+
+}
+
+// const month=3;
+// switch(month)
+// {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feburary");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//         default:
+//             console.log("default case");
+//             break;
+//               // agr break na lage to sara code excute karta hai except defualt 
+
+
+// }
+
+
+
+// TRUTHY OR FALCY
+const userEmail="h@hitesh.ai"
+if(userEmail)
+{
+    console.log("got user email");
+}
+else
+{
+    console.log("dont have an email");
+
+}
+
+// falsy value
+// false ,0,-0,bigint, 0n,"",null,undefined,nan
+
+// truthy value
+// "0" ,'false'," "[], {},function(){} 
+ 
+// if(userEmail.length()===0)
+// {
+//     console.log("array is empty");
+
+// }
+
+
+// const emptyObj={}
+// if(Object.keys(emptyObj).length()===0)
+// {
+//     console.log("Object is empty");
+// }
+
+// false==''->true
+// 0==''->true
+
+// Nullish Coalescing Operator
+let val1;
+// console.log(val1);
+// val1=5 ?? 10
+// console.log( val1);
+// val1=null ?? 10
+// console.log(val1);
+// var1=undefined?? 15
+val1=null??10??15;
+// console.log(val1);
+
+// ternary statement
+
+// condition ?true:false
+
+// const iceTeaPrice=100
+// iceTeaPrice>=80?console.log("more than 80"):console.log("less than 80");
+
+
+
+
